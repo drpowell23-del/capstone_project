cd57208b76766222fd3b83ce074e97e8e291d576
Douglas <drpowell23@gmail.com>
2026-02-11 13:59:16 -0500

    Fixed and initialized backend

--- terraform-main/infra/main.tf
+++ terraform-main/infra/main.tf
@@ -5,7 +5,7 @@ terraform {
 #  required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket  = "capstone-projectadnwpj"
+    bucket  = "capstone-projectadnwpj2"
     key     = "state/capstone-projectadnwpj2/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
